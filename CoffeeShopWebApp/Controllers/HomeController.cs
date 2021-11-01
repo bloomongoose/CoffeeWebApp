8e67372d6a0ebb22d440f24ae081eb6b1d2d75c2
radeen <radeen013@gmail.com>
2021-11-01 09:31:39 -0400

    oh yeah. it's all coming together

--- CoffeeShopWebApp/Controllers/HomeController.cs
+++ CoffeeShopWebApp/Controllers/HomeController.cs
@@ -27,6 +27,17 @@ namespace CoffeeShopWebApp.Controllers
         {
             return View();
         }
+        public IActionResult Registration()
+        {
+            return View();
+        }
+        public IActionResult Result(string First, string Last, string Email, string Password)
+        {
+            ViewData["First"] = First;
+            ViewData["Last"] = Last;
+            ViewData["Email"] = Email;
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
